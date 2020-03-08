0f647c0b2cd45cb840ba84837017b76a2d729f37
stevanus-surjadi <stevanus.surjadi@gmail.com>
2020-03-08 10:36:08 +0700

    Lesson

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
-const products = [];
+const Product = require('../models/product');
+
 
 exports.getAddProducts = (req,res,next) => {
     res.render('add-product', {
@@ -12,20 +13,22 @@ exports.getAddProducts = (req,res,next) => {
     };
 
 exports.postAddProducts = (req,res,next) => {
-    products.push({
-        title: req.body.title
-        })
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
     }
 
 exports.getProducts = (req,res,next) => {
-    res.render('shop', {
-        prods: products, 
-        pageTitle: 'Shop', 
-        path:"/", 
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true,
-
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products, 
+            pageTitle: 'Shop', 
+            path:"/", 
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true,
+    
+        });
     });
+    
 }
